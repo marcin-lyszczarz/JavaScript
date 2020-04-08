fabf568b3827394718dfc0a480f3f73984872397
Marcin Łyszczarz <lyszczarz.marcin@gmail.com>
2020-04-08 21:54:49 +0200

    toDoList

--- Zadania/projekt 12/zad 4a/app.js
+++ Zadania/projekt 12/zad 4a/app.js
@@ -1,12 +1,23 @@
+const toDoList = [];
+
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
 const listItems = document.getElementsByClassName('task');
 const input = document.querySelector('input')
 
+const refreshArray = () => {
+    ul.textContent = '';
+    toDoList.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement);
+    });
+}
 const deleteTask = (e) => {
-    e.target.parentNode.remove();
+    const indexToRemove = e.target.parentNode.dataset.key;
+    toDoList.splice(indexToRemove, 1);
     taskNumber.textContent = listItems.length;
+    refreshArray();
 }
 
 const addTask = (e) =>{
@@ -16,6 +27,9 @@ const addTask = (e) =>{
     const task = document.createElement('li');
     task.className = 'task';
     task.innerHTML = titleTask + "<button> Usuń </button>";
+    toDoList.push(task);
+    refreshArray();
+
     ul.appendChild(task);
     input.value = '';
     taskNumber.textContent = listItems.length;
